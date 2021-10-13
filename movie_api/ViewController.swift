20533a5b3dfe769c8184411c7b1f89c4335691d8
zyeongPark <piiy10141997@gmail.com>
2021-10-13 21:19:00 +0900

    Initial Commit

---
+++ movie_api/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie_api
+//
+//  Created by 박지영 on 2021/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
